28f004436da2f70c089e8c8d5ddd2c4e97cdb28f
Mufli Fadla <muflifadlayt38@gmail.com>
2022-09-12 18:36:39 +0800

    add validator request on blog

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -1,3 +1,5 @@
+const { validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     message: "Get all posts success",
@@ -17,9 +19,17 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req); //Validation error results
   const title = req.body.title;
   const image = req.body.image;
   const body = req.body.body;
+
+  if (!errors.isEmpty()) {
+    const err = new Error("Input value tidak sesuai");
+    err.errorStatus = 400;
+    err.data = errors.array();
+    throw err;
+  }
 
   res.status(201).json({
     message: "Create post success",
@@ -27,12 +37,12 @@ exports.createPost = (req, res, next) => {
       title: title,
       image: image,
       body: body,
-			created_at: "12/09/2022",
-			author: {
-				uid: 1,
-				name: "Testing",
-				email: "testing@gmail.com"
-				}
+      created_at: "12/09/2022",
+      author: {
+        uid: 1,
+        name: "Testing",
+        email: "testing@gmail.com",
+      },
     },
   });
 };
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const { body } = require("express-validator");
 const blogController = require("../controllers/blog");
 
 const router = express.Router();
 
-router.get("/posts", blogController.getPosts); //Get all posts
-router.post("/create-post", blogController.createPost); //Create a post
+//Get all posts
+router.get("/posts", blogController.getPosts);
+
+//Create a post
+router.post(
+  "/create-post",
+  [
+    body("title").isLength({ min: 8 }).withMessage("Input Title tidak sesuai"),
+    body("body").isLength({ min: 10 }).withMessage("Input Body tidak sesuai"),
+  ],
+  blogController.createPost
+);
+
 // router.put("/post/{id}", blogController.updatePosts); //Update a post
 // router.delete("/post/{id}", blogController.deletePosts); //Delete a post
 
