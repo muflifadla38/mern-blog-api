bde73388a7eeab46a76fc514e066585651d0c004
Mufli Fadla <muflifadlayt38@gmail.com>
2022-09-12 18:49:57 +0800

    add create-post & register validator request

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
+// Import Routes
 const productRoutes = require("./src/routes/products");
 const authRoutes = require("./src/routes/auth");
 const blogRoutes = require("./src/routes/blog");
@@ -15,11 +17,19 @@ app.use((req, res, next) => {
 });
 
 //Parse body request (JSON)
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 //Grouping Routes
 app.use("/v1/customer/", productRoutes);
 app.use("/v1/auth/", authRoutes);
 app.use("/v1/blog/", blogRoutes);
+
+app.use((error, req, res, next) => {
+  const status = error.errorStatus || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+  // res.json({ error: error});
+});
 
 app.listen(4000);
