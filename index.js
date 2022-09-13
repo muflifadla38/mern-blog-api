2b245cc1789cd4d9eddbec86b14823c201590a5a
Mufli Fadla <muflifadlayt38@gmail.com>
2022-09-13 15:24:14 +0800

    fix images server path

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const multer = require("multer");
+const path = require("path");
 const dotenv = require("dotenv").config({ path: ".env" });
 
 // Import Routes
@@ -30,7 +31,7 @@ const fileStorage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     cb(null, new Date().getTime() + "-" + file.originalname); //File name format
-  }
+  },
 });
 
 //Set file filter (only accept image file)
@@ -42,10 +43,13 @@ const fileFilter = (req, file, cb) => {
   filter ? cb(null, true) : cb(null, false);
 };
 
-// Upload image file to root/image with file filter and single file only (using req.body.image)
+// Upload image file (root/image), filter & single file (req.body.image)
 app.use(
   multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
 );
+
+//Set static folder (root/images)
+app.use("/images/", express.static(path.join(__dirname, "images")));
 
 //Grouping Routes
 app.use("/v1/customer/", productRoutes);
